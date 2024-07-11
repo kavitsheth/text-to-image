7051e3cd40881d6a136b228c8c41c42f03ae59d6
kavitsheth <shethkavit2@gmail.com>
2024-07-12 02:29:16 +0530

    Changes done by kavit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,68 +12,6 @@ use Nette\Utils\Image;
 
 class MainController extends Controller
 {
-    //
-
-    // function index(Request $request){
-
-    //     $curl = curl_init();
-
-    //     curl_setopt_array($curl, array(
-    //     CURLOPT_URL => 'https://api.stability.ai/v2beta/stable-image/generate/ultra',
-    //     CURLOPT_RETURNTRANSFER => true,
-    //     CURLOPT_ENCODING => '',
-    //     CURLOPT_MAXREDIRS => 10,
-    //     CURLOPT_TIMEOUT => 0,
-    //     CURLOPT_FOLLOWLOCATION => true,
-    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //     CURLOPT_CUSTOMREQUEST => 'POST',
-    //     CURLOPT_POSTFIELDS =>'{
-    //     "prompt": "Asronaut driving a spaceship",
-    //     "output_format": "webp",
-    //     }',
-    //     CURLOPT_HTTPHEADER => array(
-    //         'Content-Type: application/json',
-    //         'authorization: Bearer sk-EfdwMQqfEy0OVl1boSuGDgpPhk2kMSAsEY0I9fluKti40DuS',
-    //         'accept: image/*',
-    //     ),
-    //     ));
-
-    //     $response = curl_exec($curl);
-
-    //     if (curl_errno($curl)) {
-    //         $error_msg = curl_error($curl);
-    //         dd($error_msg);
-    //     }
-
-    //     curl_close($curl);
-    //     // dd($curl)
-    //     echo $response;
-
-    // }
-
-    // public function postGuzzleRequest()
-    // {
-    //     $client = new Client();
-
-    //     $response = $client->post('https://api.stability.ai/v2beta/stable-image/generate/sd3', [
-    //         'headers' => [
-    //             'authorization' => 'Bearer sk-GolpFBhIEXHQHwRwch1vGwZzf14U1FbU4Ds4x0cCZtgDFBMW',
-    //             'accept'        => 'image/*'
-    //         ],
-    //         'multipart' => [
-    //             [
-    //                 'name' => 'prompt',
-    //                 'contents' => 'Lighthouse on a cliff overlooking the ocean'
-    //             ],
-    //             [
-    //                 'name' => 'output_format',
-    //                 'contents' => 'jpeg'
-    //             ]
-    //         ]
-    //     ]);
-
-    //     return $response;
-    // }
 
     public function TextToImage(Request $request) {
 
@@ -126,19 +64,6 @@ class MainController extends Controller
 
         $responseData = json_decode($response, true); // Assuming $response is your HTTP response object
 
-        // if(Session::get('image')){
-        //     Session::forget('image');
-        //     Session::put('image',$responseData['output'][0]);
-        // }else{
-        //     Session::put('image',$responseData['output'][0]);
-        // }
-
-        // return to_route('index');
-
-
-
-
-
 
         $promptInsert = 'App\\Models\\Prompt'::create([
             'prompt'=>$request->prompt,
@@ -164,26 +89,6 @@ class MainController extends Controller
                 'source'=>$filename,
             ]);
 
-
-            // // Retrieve the image URL from the request
-            // $imageUrl = $value;
-
-
-            // $filename = pathinfo($imageUrl)['basename'];
-
-
-            //     // Store the downloaded image in the storage directory
-            //     $storedPath = Storage::put('public/uploads/' . $filename, $imageUrl);
-
-            //     // If you need to generate a URL for accessing the stored image publicly
-
-            //     $imageInsert = 'App\\Models\\Image'::create([
-            //         'prompt_id'=>$promptInsert->id,
-            //         'source'=>$filename,
-            //     ]);
-
-                // return response()->json(['error' => 'success.']);
-
         }
 
 
@@ -204,11 +109,6 @@ class MainController extends Controller
 
     public function test() {
 
-        // return response(["https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/temp/3f9d3a52-d4ac-483b-b868-e5f34de6b656-0.png",
-        // "https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/temp/3f9d3a52-d4ac-483b-b868-e5f34de6b656-1.png",
-        // "https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/temp/3f9d3a52-d4ac-483b-b868-e5f34de6b656-2.png",
-        // "https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/temp/3f9d3a52-d4ac-483b-b868-e5f34de6b656-3.png"]);
-
 
         $promptInsert = 'App\\Models\\Prompt'::create([
             'prompt'=>"This is the prompt for testing.",
@@ -227,18 +127,6 @@ class MainController extends Controller
             $imageUrl = $value;
 
 
-            // Image::PNG($value);
-
-            // pathinfo($imageUrl);
-
-            // dd(pathinfo($imageUrl)['basename']);
-            // dd(pathinfo($imageUrl));
-
-            // dd($imageUrl);
-
-            // Initialize Guzzle client
-            // $client = new Client();
-
             $filename = uniqid() . '_' . pathinfo($imageUrl)['basename'];
 
 
@@ -254,37 +142,6 @@ class MainController extends Controller
                 ]);
 
                 return response()->json(['error' => 'success.']);
-
-            // try {
-            //     // Send a GET request to the image URL to download it
-            //     // $response = $client->get($imageUrl);
-
-            //     // image::save()
-
-            //     // dd($response);
-
-            //     // Generate a unique filename for storing the image
-            //     $filename = uniqid() . '_' . pathinfo($imageUrl)['basename'];
-
-
-            //     // Store the downloaded image in the storage directory
-            //     $storedPath = Storage::put('public/uploads/images' . $filename, $imageUrl);
-
-            //     // If you need to generate a URL for accessing the stored image publicly
-            //     $url = Storage::url($storedPath);
-
-            //     $imageInsert = 'App\\Models\\Image'::create([
-            //         'prompt_id'=>$promptInsert->id,
-            //         'image'=>$url,
-            //     ]);
-
-            //     return response()->json(['error' => 'success.']);
-
-            // } catch (\Exception $e) {
-            //     // Handle any exceptions that may occur during the request
-            //     return response()->json(['error' => 'Failed to download image from URL.'], 500);
-            // }
-
 
         }
 
